d671554664a3074c2845cdbe1eea38bb5e9fed88
rayiooo <rayiooo@foxmail.com>
2018-06-28 16:36:57 +0800

    start write.

---
+++ src/client/LoginFrame.java
@@ -0,0 +1,14 @@
+package client;
+
+import javax.swing.JFrame;
+
+public class LoginFrame extends JFrame{
+	public LoginFrame(){
+		super("Ô¼ - µÇÂ½/×¢²á");
+		
+		// necessary settings
+		this.setSize(300, 400);
+		this.setResizable(false);
+		this.setVisible(true);
+	}
+}
